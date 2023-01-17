ae432eebf5acba5411fe2e95aa15319bbebc6a67
whopes <hopes.w@etoncollege.org.uk>
2023-01-17 11:37:51 +0000

    Initial Commit

---
+++ car EW/car_EWApp.swift
@@ -0,0 +1,17 @@
+//
+//  car_EWApp.swift
+//  car EW
+//
+//  Created by Hopes, William (AMM) on 17/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct car_EWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
