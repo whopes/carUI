ba2d71b5481cea67a0fe475810c0c84b6588bf7b
whopes <hopes.w@etoncollege.org.uk>
2023-01-17 12:35:42 +0000

    started

---
+++ car EW/Car.swift
@@ -0,0 +1,16 @@
+//
+//  Car.swift
+//  car EW
+//
+//  Created by Hopes, William (AMM) on 17/01/2023.
+//
+
+import Foundation
+
+struct Car {
+    let make: String
+    let model: String
+    var topSpeed: Int
+    var acceleration: Double
+    var handling: Int
+}
--- car EWTests/car_EWTests.swift
+++ car EWTests/car_EWTests.swift
@@ -5,32 +5,21 @@
 //  Created by Hopes, William (AMM) on 17/01/2023.
 //
 
-import XCTest
-@testable import car_EW
+import CTest
 
 class car_EWTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    func testNewCarGivesMeACarWithAllAttributesSet() {
+        //arrange
+        //act
+        let car = Car(make: "Audi", model: "R-8", topSpeed: 125, acceleration: 7.7, handling: 5)
+        //assert
+        XCTAssertEqual(car.make, "Audi")
+        XCTAssertEqual(car.model, "R-8")
+        XCTAssertEqual(car.topSpeed, 125)
+        XCTAssertEqual(car.acceleration, 7.7)
+        XCTAssertEqual(car.handling, 5)
+        
     }
 
 }
